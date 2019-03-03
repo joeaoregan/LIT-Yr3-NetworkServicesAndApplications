4e64868f87ba9882de4cd4ef89729ca840657921
Joe O'Regan <joeaoregan@yahoo.ie>
2019-03-03 23:01:53 +0000

    15/09/2016 Lab 1

---
+++ Lab-OriginalFiles/README.md
@@ -0,0 +1,14 @@
+# Original Files
+
+### Client & Server Need:
+
+* DieWithMessage.c
+
+### TCPEchoClient.c Needs:
+
+* TCPClientUtility.c
+
+### TCPServerUtility.c Needs:
+
+* TCPServerUtility.c
+* AddressUtility.c
---
+++ Lab1/TCPEchoServer4.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include "Practical.h"
+
+static const int MAXPENDING = 5; // Maximum outstanding connection requests
+
+int main(int argc, char *argv[]) {//run on command line = "echoSvr <port>";argc = 2 command and parameter-  argv[0] = echoSvr and argv[1] = <port>
+
+  if (argc != 2) // Test for correct number of arguments
+    DieWithUserMessage("Parameter(s)", "<Server Port>");
+
+  in_port_t servPort = atoi(argv[1]); // First arg:  local port ascii to integer
+
+  // Create socket for incoming connections
+  int servSock; // Socket descriptor for server
+  if ((servSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+    DieWithSystemMessage("socket() failed");
+
+  // Construct local address structure
+  struct sockaddr_in servAddr;                  // Local address; internet socket address structure
+  memset(&servAddr, 0, sizeof(servAddr));       // Zero out structure
+  servAddr.sin_family = AF_INET;                // IPv4 address family
+  servAddr.sin_addr.s_addr = htonl(INADDR_ANY); // Any incoming interface; host to network long[integer]
+  servAddr.sin_port = htons(servPort);          // Local port; host to network short[integer]
+
+  // Bind to the local address
+  if (bind(servSock, (struct sockaddr*) &servAddr, sizeof(servAddr)) < 0)//cast servaddr as generic socket address structure
+    DieWithSystemMessage("bind() failed");
+
+  // Mark the socket so it will listen for incoming connections
+  if (listen(servSock, MAXPENDING) < 0)
+    DieWithSystemMessage("listen() failed");
+
+  for (;;) { // Run forever
+    struct sockaddr_in clntAddr; // Client address
+    // Set length of client address structure (in-out parameter)
+    socklen_t clntAddrLen = sizeof(clntAddr);
+
+    // Wait for a client to connect
+    int clntSock = accept(servSock, (struct sockaddr *) &clntAddr, &clntAddrLen);
+    if (clntSock < 0)
+      DieWithSystemMessage("accept() failed");
+
+    // clntSock is connected to a client!
+
+    char clntName[INET_ADDRSTRLEN]; // String to contain client address
+    if (inet_ntop(AF_INET, &clntAddr.sin_addr.s_addr, clntName,
+        sizeof(clntName)) != NULL)
+      printf("Handling client %s/%d\n", clntName, ntohs(clntAddr.sin_port));
+    else
+      puts("Unable to get client address");
+
+    HandleTCPClient(clntSock);
+  }
+  // NOT REACHED
+}
